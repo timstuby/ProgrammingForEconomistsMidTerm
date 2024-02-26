34f89eaffb7d60829270d215e7d6ac49cc32f2e2
timstuby <tstuby@icloud.com>
2024-02-26 18:03:50 +0100

    q8 (was done before time end)

---
+++ q3.py
@@ -0,0 +1,14 @@
+a = 16
+
+a = a // 2
+
+print(a**2)
+
+a = a + 11
+
+print(a+1)
+
+a = a - 3
+
+
+print(a)
--- q8.py
+++ q8.py
@@ -1,6 +1,6 @@
-url = input('Enter anything, preferably a URL')
-
-if url.startswith("http://") or url.startswith("https://") or url.startswith("www.") and url.__contains__(".com") or url.__contains__(".edu") or url.__contains__(".org") or url.__contains__(".gov"):
-    print("This is a valid URL")
-else:
-    print("This is not a valid URL")
+url = 'http://www.pythonchallenge.com/pc/def'
+def urlcheck(url):
+    if url.startswith("http://") or url.startswith("https://") or url.startswith("www.") and url.__contains__(".com") or url.__contains__(".edu") or url.__contains__(".org") or url.__contains__(".gov"):
+        print("This is a valid URL")
+    else:
+        print("This is not a valid URL")
